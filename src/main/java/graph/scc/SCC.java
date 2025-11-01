f6a84b6b0afa45531dd4e37d96e12e7569e18c08
ddsheyh <243077@astanait.edu.kz>
2025-11-01 19:02:17 +0500

    implement SCC with Kosarjau algorithm. add SCC finding with 2 dfs passes

--- src/main/java/graph/scc/SCC.java
+++ src/main/java/graph/scc/SCC.java
@@ -14,5 +14,47 @@ public class SCC {
                 dfs1(graph, i, visited, stack);
             }
         }
+        Graph transposed = transpose(graph);
+
+        Arrays.fill(visited, false);
+        List<List<Integer>> components = new ArrayList<>();
+
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
+            if (!visited[node]) {
+                List<Integer> component = new ArrayList<>();
+                dfs2(transposed, node, visited, component);
+                components.add(component);
+            }
+        }
+        return components;
+    }
+
+    private static void dfs1(Graph graph, int node, boolean[] visited, Stack<Integer> stack) {
+        visited[node] = true;
+        for (int neighbor : graph.adj.get(node)) {
+            if (!visited[neighbor]) {
+                dfs1(graph, neighbor, visited, stack);
+            }
+        }
+        stack.push(node);
+    }
+    private static void dfs2(Graph graph, int node, boolean[] visited, List<Integer> component) {
+        visited[node] = true;
+        component.add(node);
+        for (int neighbor : graph.adj.get(node)) {
+            if (!visited[neighbor]) {
+                dfs2(graph, neighbor, visited, component);
+            }
+        }
+    }
+    private static Graph transpose(Graph graph) {
+        Graph transposed = new Graph(graph.n);
+        for (int u = 0; u< graph.n; u++) {
+            for (int v : graph.adj.get(u)) {
+                transposed.addEdge(v, u, graph.weights.get(u +", " +v));
+            }
+        }
+        return transposed;
     }
 }
